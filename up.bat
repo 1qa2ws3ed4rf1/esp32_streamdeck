d7e9f349886b9a30d1d6a089d7510beb86e633b6
1qa2ws3ed4rf1 <killjsj@outlook.com>
2024-04-20 14:08:27 +0800

    hardware upload

--- up.bat
+++ up.bat
@@ -1,7 +1,7 @@
 git remote add origin https://github.com/1qa2ws3ed4rf1/esp32_streamdeck.git
 git add .
 set /p comm=Please input commit:
-git commit -m %comm%
+git commit -m "%comm%"
 git pull
 git push -u origin main
 pause

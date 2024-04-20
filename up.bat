89c777d5dac5c0ccbd2fe2a21d805480642925cf
1qa2ws3ed4rf1 <killjsj@outlook.com>
2024-04-20 23:13:58 +0800

    echo test

--- up.bat
+++ up.bat
@@ -1,8 +1,28 @@
+
 git remote add origin https://github.com/1qa2ws3ed4rf1/esp32_streamdeck.git
-set /p mode = mode: 1=add all thing 2=input your command(like be git add streamdeck.ino)
+if  not "%1"==0  (goto file)
+:back
+set /p mode = mode: 1=add all thing another=input your command(like be git add streamdeck.ino):
+if "%mode%" == 1  (goto all)
+if NOT "%mode%" == 1 (goto command)
+
+:command
+set /p run=command:
+%run%
+goto upload
+
+:all
 git add .
+goto upload
+
+:upload
 set /p comm=Please input commit:
 git commit -m "%comm%"
 git pull
 git push -u origin main
 pause
+exit
+
+:file
+git add "%1"
+goto back

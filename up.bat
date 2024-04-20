1141d39f1b13285a9ec31e224d66d4980965c1b7
1qa2ws3ed4rf1 <killjsj@outlook.com>
2024-04-20 23:25:04 +0800

    Update code and test up.bat

--- up.bat
+++ up.bat
@@ -1,7 +1,6 @@
-
 @echo off
 git remote add origin https://github.com/1qa2ws3ed4rf1/esp32_streamdeck.git
-if  defined "%1"  (goto file)
+if  not "%1" == ""  (goto file)
 :back
 set /p mode=mode: 1=add all thing another=input your command(like be git add streamdeck.ino):
 if "%mode%" == "1"  (goto all)
@@ -25,5 +24,6 @@ pause
 exit
 
 :file
+echo %1 will upload to github!
 git add "%1"
 goto back

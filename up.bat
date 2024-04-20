62cf85215f6c9a80e0d665b79d6113116dab77cf
1qa2ws3ed4rf1 <killjsj@outlook.com>
2024-04-20 23:19:55 +0800

    test

--- up.bat
+++ up.bat
@@ -1,6 +1,7 @@
 
+@echo off
 git remote add origin https://github.com/1qa2ws3ed4rf1/esp32_streamdeck.git
-if  not "%1"==0  (goto file)
+if  defined "%1"  (goto file)
 :back
 set /p mode=mode: 1=add all thing another=input your command(like be git add streamdeck.ino):
 if "%mode%" == "1"  (goto all)

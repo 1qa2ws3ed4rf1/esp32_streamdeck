604527219f4e9536f580a10f67fe4d37d92945cd
1qa2ws3ed4rf1 <killjsj@outlook.com>
2024-08-27 16:26:37 +0800

    new file:   software/godot/Serial.cs 	new file:   software/godot/button1.gd 	new file:   software/godot/button2.gd     new file:   software/godot/button3.gd 	new file:   software/godot/button4.gd 	modified:   software/godot/node_2d.gd 	modified:   software/godot/node_2d.tscn

---
+++ software/godot/Serial.cs
@@ -0,0 +1,146 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+public partial class Serial: Node2D {
+    private SerialPort port = new SerialPort();
+    private List < PortC > fail = new List < PortC > ();
+    private
+    const string Eof = "Eof";
+    [Signal]
+    public delegate void data_receivedEventHandler(string data);
+
+    public Thread thread;
+    public bool reset;
+    private bool Skip;
+    public class PortC: IEquatable < PortC > {
+        public string portName;
+        public int time;
+        public override bool Equals(object obj) {
+            if (obj == null) return false;
+            PortC objPo = obj as PortC;
+            if (objPo == null) return false;
+            else return Equals(objPo);
+        }
+        public override string ToString() {
+            if (time == 0) {
+                return portName + " a";
+            } else {
+                return portName + " b";
+            }
+        }
+        public bool Equals(PortC other) {
+            if (other == null) return false;
+            return this.portName.Equals(other.portName);
+        }
+
+        public override int GetHashCode() {
+            return 0;
+        }
+    }
+    private TaskCompletionSource < bool > tscb = new TaskCompletionSource < bool > ();
+    private TaskCompletionSource < bool > tscl = new TaskCompletionSource < bool > ();
+    private TaskCompletionSource < string > tscs = new TaskCompletionSource < string > ();
+    private string raw;
+
+    public override void _Ready() {
+        GD.Print("try");
+        thread = new Thread(_process);
+        thread.Start();
+    }
+
+    public async void _process() {
+        while (true)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach(string portName in ports) {
+                if (Checkport(portName, 0)) {
+                    continue;
+                }
+
+                try {
+                    if(!port.IsOpen){
+                        
+                        port.PortName = portName;
+                        port.BaudRate = 512000;
+                        port.DataBits = 8;
+                        port.StopBits = StopBits.One;
+                        port.Encoding = Encoding.UTF8;
+                    GD.Print("a");
+                    port.DataReceived += data;
+                    GD.Print("b");
+                    port.Open();
+                    }
+                    bool hasData = await tscb.Task;
+                    GD.Print("c");
+                    if (hasData) {
+                        string data = tscs.Task.Result;
+                        // EmitSignal( nameof(data_received), data);
+                        this.CallDeferred("emit_signal", "data_received", data);
+                        break;
+                    }
+                    await tscl.Task;
+                    port.Close();
+                } catch (Exception ex) {
+                    GD.Print($"Error accessing {port.PortName}: {ex.Message} AT {ex.Source} {ex.StackTrace}");
+                    Checkport(port.PortName, 1);
+                }
+            
+        }
+        }
+    }
+
+    private void data(object sender, SerialDataReceivedEventArgs e) {
+        try {
+            SerialPort _p = (SerialPort) sender;
+            if (_p.IsOpen) {
+                tscl.TrySetResult(false);
+                string Data = _p.ReadTo(Eof);
+                tscl.TrySetResult(true);
+                tscs.TrySetResult(Data);
+                tscb.TrySetResult(true);
+            };
+        } catch (Exception ex) {
+            GD.Print($"Error accessing {port.PortName}: {ex.Message} AT {ex.Source} {ex.StackTrace}");
+        }
+    }
+
+
+    private bool Checkport(string portName, int v) {
+        if (v == 1) {
+            if (fail.Exists(p => p.portName == portName)) {
+                if (fail.Exists(p => p.time == 0)) {
+                    int dex = fail.FindIndex(p => p.portName == portName);
+                    fail.Add(new PortC() {
+                        portName = fail[dex].portName, time = 1
+                    });
+                    fail.RemoveAt(dex);
+                } else {
+                    int dex = fail.FindIndex(p => p.portName == portName);
+                    GD.PrintErr("!!! AT " + portName + "!!! " + "time:" + fail[dex].time + "dex: " + dex);
+                }
+            } else {
+                fail.Add(new PortC() {
+                    portName = portName, time = 0
+                });
+            }
+            return false;
+        } else {
+            int dex = fail.FindIndex(p => p.portName == portName);
+            if (dex == -1) {
+                return false;
+            }
+            if (fail[dex].time == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+}
--- software/godot/node_2d.gd
+++ software/godot/node_2d.gd
@@ -1,4 +1,20 @@
 extends Control
+signal b1
+signal b2
+signal b3
+signal b4
+signal b5
+signal b6
+signal b7
+signal b8
+signal b9
+signal b10
+signal b11
+signal b12
+signal b13
+signal b14
+signal b15
+signal b16
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,3 +23,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_node_2d_data_received(data):
+	
+	pass # Replace with function body.
